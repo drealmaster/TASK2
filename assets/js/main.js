345075a1f147405a32641f933df338676a0de10f
okere chibuike <masterokere0@gmail.com>
2021-08-18 17:54:30 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+// ======================= TASK 1 ===============================
+
+// program that prints your name
+
+const myName = (names) => {
+    console.log(names);
+}
+
+myName('Master okere');
+
+
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
